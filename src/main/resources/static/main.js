db6d458d4b53d138f7698e26c59e5f6be666192d
Harvey <hmunoz_9016@email.ric.edu>
2022-07-09 00:26:22 -0400

    styling task page

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,8 +1,10 @@
 
 let x;
-// opening modal and setting previous information inside inputs
+
 $(document).ready(function() {
 
+
+    // opening modal and setting previous information inside inputs
     $('.table .btn-warning').on('click', function (event) {
         event.preventDefault();
 
@@ -20,25 +22,64 @@ $(document).ready(function() {
     });
 
 
+   // const emptyInput = false;
+    $('#addBtn').on('click', function(event) {
 
-    x = 0;
-    // when user clicks on the 'Finished Button'
-    $('.table .btn-success').on('click',function(event) {
-        event.preventDefault();
-        const status = document.getElementById("statusId");
 
-        if(x === 0) {
-            status.innerText = "Complete";
-            x = 1;
-        } else {
-            status.innerText = "In progress";
-            x = 0;
+       // on button click , console.log when input field of id (inputTaskName) is empty
+        console.log("Button clicked");
+
+        const taskInput = document.getElementById("taskDesc");
+
+        if(taskInput.value.length === 0) {
+            alert("Description cannot be empty!");
         }
 
 
-        console.log(x);
-
     });
+
+
+    // when user click on the add button and the input is empty then provide a message
+    // $('##addTask').validate({
+    //
+    //     rules: {
+    //         task: "required",
+    //         description: {
+    //             required: true,
+    //             minlength: 1
+    //         },
+    //         message: {
+    //             task: "Task must not be empty!"
+    //         }
+    //
+    //     }
+    //
+    //     }
+    // )
+
+
+
+
+
+
+    // x = 0;
+    // // when user clicks on the 'Finished Button'
+    // $('.table .btn-success').on('click',function(event) {
+    //     event.preventDefault();
+    //     const status = document.getElementById("statusId");
+    //
+    //     if(x === 0) {
+    //         status.innerText = "Complete";
+    //         x = 1;
+    //     } else {
+    //         status.innerText = "In progress";
+    //         x = 0;
+    //     }
+    //
+    //
+    //     console.log(x);
+    //
+    // });
 
 
 
